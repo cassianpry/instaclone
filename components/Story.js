82b00fe8a3ad511bc431e4c6b747be227a7a448f
cassianpry <crmarinello@gmail.com>
2022-11-09 12:16:11 -0300

    Estilos na story section e adicionado plugin tailwind-scrollbar

--- components/Story.js
+++ components/Story.js
@@ -2,9 +2,15 @@ import Image from 'next/image';
 
 export default function Story({ img, username }) {
   return (
-    <div>
-      <Image src={img} alt={username} width={100} height={100} />
-      <p>{username}</p>
+    <div className="text-center">
+      <Image
+        className="rounded-full p-[1.5px] border-2 border-red-500 hover:scale-110 transition-transform duration-200 ease-out"
+        src={img}
+        alt={username}
+        width={100}
+        height={100}
+      />
+      <p className="text-xs w-14 truncate ">{username}</p>
     </div>
   );
 }
