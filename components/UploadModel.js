c8a3d0b055dec9940f6084faf5989f9f724d9fc5
cassianpry <crmarinello@gmail.com>
2022-11-11 13:04:12 -0300

    Finalizado design do componente Modal

--- components/UploadModel.js
+++ components/UploadModel.js
@@ -1,19 +1,71 @@
-import { modalState } from '../atom/modalAtom';
+import { useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
+import { modalState } from '../atom/modalAtom';
 import Modal from 'react-modal';
+import { CameraIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function UploadModel() {
   const [open, setOpen] = useRecoilState(modalState);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const filePickerRef = useRef(null);
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
   return (
     <div>
       {open && (
         <Modal
-          className="bg-white border-2 rounded-lg shadow-md max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%]"
+          className="bg-white border-2 rounded-lg shadow-md max-w-xl w-[100%] absolute p-6 top-56 left-[50%] translate-x-[-50%] focus:ring-0"
           isOpen={open}
-          onRequestClose={() => setOpen(false)}
+          onRequestClose={() => {
+            setOpen(false);
+            setSelectedFile(null);
+          }}
         >
           <div className="flex flex-col justify-center items-center h-[100%]">
-            <h1>Modal</h1>
+            {selectedFile ? (
+              <Image
+                className="w-full max-h-[75%]"
+                src={selectedFile}
+                alt={selectedFile}
+                width="0"
+                height="0"
+                onClick={() => setSelectedFile(null)}
+              />
+            ) : (
+              <CameraIcon
+                className="cursor-pointer h-14 bg-blue-200 p-2 rounded-full border-2 text-blue-500"
+                onClick={() => filePickerRef.current.click()}
+              />
+            )}
+
+            <input
+              type="file"
+              hidden
+              ref={filePickerRef}
+              onChange={addImageToPost}
+            />
+            <input
+              className="m-4 border-none text-center w-full focus:ring-0"
+              type="text"
+              max="150"
+              placeholder="Escreval algo legal..."
+            />
+            <button
+              className="w-full rounded-md bg-blue-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+              disabled
+            >
+              Postar
+            </button>
           </div>
         </Modal>
       )}
